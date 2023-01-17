1b4b0fa81dbe8fb8ee6dd37508cfd55560c7816c
ShivangRawat30 <rawatshivang30@gmail.com>
2023-01-17 23:36:12 +0530

    commit2

--- script.js
+++ script.js
@@ -199,17 +199,25 @@ const wordList = [
 const inputs = document.querySelector(".inputs"),
 resetBtn = document.querySelector(".reset-btn"),
 hint = document.querySelector(".hint span"),
+guessesLeft = document.querySelector(".guess-left span"),
+wrongLetter = document.querySelector(".wrong-letter span"),
 typingInput = document.querySelector(".typing-input");
 
-let word;
+let word, maxGuesses ,corrects = [] ,incorrects = [];
 
 function randomWord() {
     // getting random object from wordList
     let ranObj = wordList[Math.floor(Math.random() * wordList.length)];
     word = ranObj.word; // getting word of random object
-    console.log(ranObj);
+    maxGuesses = 8;
+    corrects = []; 
+    incorrects = [];
+    console.log(word);
 
     hint.innerHTML = ranObj.hint;
+    guessesLeft.innerHTML = maxGuesses;
+    wrongLetter.innerText = incorrects;
+
 
     let html = "";
     for (let i = 0; i < word.length; i++) {
@@ -223,22 +231,43 @@ randomWord();
 
 function initGame(e) {
     let key = e.target.value;
-    if(key.match(/^[A-Za-z]+$/)){
-    console.log(key);
+    if(key.match(/^[A-Za-z]+$/) && !incorrects.includes(` ${key} `) && !corrects.includes(key)){
     if(word.includes(key)){ //if user letter found in the word
           for (let i = 0; i < word.length; i++) {
             //showing matched letter in the input value
              if (word[i] === key) {
-                inputs.querySelectorAll("input")[i],value = key;
+                corrects.push(key);
+                inputs.querySelectorAll("input")[i].value = key;
              }
             
           }
     } else {
-          console.log("letter not found")
+          maxGuesses--; // decrement maxGuesses by 1
+          incorrects.push(` ${key} `);
     }
+    guessesLeft.innerText = maxGuesses;
+    wrongLetter.innerText = incorrects;
     }
+    typingInput.value = "";
+
+   setTimeout(() => {
+    if(corrects.length == word.length){ // if user found all letters
+        alert(`Congrats! You found the word`);
+        randomWord(); // calling randomWord func, so the game reset
+     }
+ 
+     else if(maxGuesses < 1 ){ // if user couldn't found all letters 
+        alert("Game Over! You don't have reamaining guesses");
+        for (let i = 0; i < word.length; i++) {
+             // showing matched letter in the input value
+             inputs.querySelectorAll("input")[i].value = word[i];
+          
+       }
+     }
+   });
 }
 
 resetBtn.addEventListener("click", randomWord);
 typingInput.addEventListener("input", initGame);
+inputs.addEventListener("click", () => typingInput.focus());
 document.addEventListener("keydown", () => typingInput.focus());
